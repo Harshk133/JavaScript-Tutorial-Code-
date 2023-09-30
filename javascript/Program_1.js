15d3500039359cf2a5d903483f30db474e22870d
Harsh <harshmkale.2004@gmail.com>
2023-09-30 21:31:36 +0530

    #1 Added Program_1.js File

---
+++ javascript/Program_1.js
@@ -0,0 +1,32 @@
+// Ways to write your javascript script!!
+
+// Write
+// * document.write();
+// document.write("<br>This file is added!!");
+document.write(`<h1 style="color: orange;">Hello, world!</h1>`);
+document.write("<tt>This is my JavaScript Code!</tt>")
+
+// Console
+// * Console.log();
+var a = "my variable!";
+a = { key: "value" };
+console.group("Console!!");
+// console.groupCollapsed();
+console.log("This is a console!"); // String
+console.log(77); // Number
+console.log(true); // 1(true) | 0(false)
+console.log([1,2,34,5,6,7]); // arr
+console.error("This is a fatal error!!");
+console.warn("This is a warning!!");
+console.info("This is informations!");
+console.log({ name: "Harsh Kale", msg: "Hello, world!" });
+console.table({ name: "Harsh Kale", msg: "Hello, world!" });
+console.log(a);
+console.dir(a);
+var style = `color: orange; font-weight: bold;`;
+console.log("%cWelcome, to my channel!!", style);
+console.groupEnd();
+
+
+
+
